40681e158ebe8b81feca581fad29f310b272b6e3
Matthias-Ubuntu <na.matthias@hotmail.com>
2016-05-16 23:16:39 +0200

    angular test

---
+++ PO.Application.Web/script.js
@@ -0,0 +1,34 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('paymentOverviewApp', ['ngRoute']);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/payments', {
+				templateUrl : 'pages/payments.html',
+				controller  : 'paymentsController'
+			})
+
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+
+    
+    
